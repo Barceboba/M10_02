d12c9d03059df2791505be9489d175b6c4ad4927
Barceboba <miquel@barceboba.com>
2021-09-23 16:43:57 +0200

    mitga clase

--- e1_components/src/App.vue
+++ e1_components/src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <HelloWorld msg="A boinazos entre padre e hijo"/>
+    <Fill msg="Al llarg del cami plegats"/>
+    <Pare msg="La vostra covardia és esfereïdoraa"/>  </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Pare from './components/Pare.vue'
+import Fill from './components/Fill.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Fill,
+    Pare
+
   }
 }
 </script>
